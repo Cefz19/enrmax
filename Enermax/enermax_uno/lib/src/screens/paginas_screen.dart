9d64fcd551823380a03307cbb735bccb8b446bfc
Cefz19 <molanddise@gmail.com>
2022-10-25 18:39:56 -0500

    Ya no se tienen errores ya esta lista para ejecutar v4

--- Enermax/enermax_uno/lib/src/screens/paginas_screen.dart
+++ Enermax/enermax_uno/lib/src/screens/paginas_screen.dart
@@ -1,5 +1,3 @@
-import 'package:enermax_uno/src/screens/distribucion.dart';
-
 export 'package:enermax_uno/src/screens/distribucion.dart';
 export 'package:enermax_uno/src/screens/manifiesto.dart';
 export 'package:enermax_uno/src/screens/transito.dart';
