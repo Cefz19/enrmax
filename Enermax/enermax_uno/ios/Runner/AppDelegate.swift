062eefe28f7fbc44fa641e3e2431fc81f165537b
Cefz19 <molanddise@gmail.com>
2022-10-26 09:13:14 -0500

    Se modificaron las carpetas de android y Iso v5

--- Enermax/enermax_uno/ios/Runner/AppDelegate.swift
+++ Enermax/enermax_uno/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDRv2ENELq_mXm-SV_NRlg5k9azu6op9wY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
